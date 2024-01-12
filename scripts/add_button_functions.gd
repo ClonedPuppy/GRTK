b6b9bb75cfb2db2039cd1875d5074232c7050d57
Peter Riel <peter@riel.nu>
2024-01-12 21:47:06 +0800

    Moved most of the button logic to the button script

--- scripts/add_button_functions.gd
+++ scripts/add_button_functions.gd
@@ -3,7 +3,7 @@ extends GridMap
 @onready var lever = $"../lever"
 @onready var ui_meshes = self.mesh_library
 var button_number = 0
-var current_sender
+var current_area
 
 func _ready():
 	if ui_meshes:
@@ -19,50 +19,22 @@ func _ready():
 
 func create_manipulator_for_cell(cell: Vector3, cell_name: String):
 	var area = Area3D.new()
-	current_sender = area
+	current_area = area
 	area.name = "Button_" + str(button_number)
-	add_child(area)
-
+	
 	# Calculate the global transform for the cell
 	var cell_transform = Transform3D()
 	cell_transform.origin = cell * cell_size + Vector3(0.01, 0.0025, 0.01)
 	area.global_transform = global_transform * cell_transform
-
-	# Add a collision node
-	var collision_shape = CollisionShape3D.new()
-	collision_shape.name = "Collision_" + str(button_number)
-	area.add_child(collision_shape)
-
-	# Add a shape to the collision node, assuming button size is 0.005 x 0.005
-	var shape = CylinderShape3D.new()
-	shape.height = 0.005
-	shape.radius = 0.005
-	collision_shape.shape = shape
-
-	area.set_meta("cell", cell)
-	area.set_meta("name", cell_name)
 	
-	# Add a MeshInstance3D with the leverage mesh at the same location
-	var mesh_instance = MeshInstance3D.new()
-	var mesh = lever.mesh
-	if mesh:
-		mesh_instance.mesh = mesh
-	else:
-		print("Failed to load mesh from:", lever)
-		return
+	add_child(area)
 
-	var corrected_rotation = Quaternion(Vector3(1, 0, 0), deg_to_rad(-90))
-	mesh_instance.transform = Transform3D(corrected_rotation, Vector3(0,-0.0025,0))
-	mesh_instance.name = "Lever_" + str(button_number)
-
-	area.add_child(mesh_instance)
-	
 	# Add script
 	var script_path = "res://scripts/btn.gd"
 	var script = load(script_path)
 	if script:
 		area.script = script
 		if area.has_method("init"):
-			area.call("init", area)
+			area.call("init", area, button_number)
 	else:
 		print("Failed to load script:", script_path)
--- scripts/btn.gd
+++ scripts/btn.gd
@@ -2,11 +2,36 @@ extends Area3D
 
 var area
 
-func init(area_node):
+func init(area_node: Area3D, cell_no: int):
 	print("run..")
 	area = area_node
 	area_node.connect("body_entered", Callable(self, "_on_Area3D_body_entered"))
+		# Add a collision node
+	var collision_shape = CollisionShape3D.new()
+	collision_shape.name = "Collision_" + str(cell_no)
+	area.add_child(collision_shape)
 
+	# Add a shape to the collision node, assuming button size is 0.005 x 0.005
+	var shape = CylinderShape3D.new()
+	shape.height = 0.005
+	shape.radius = 0.005
+	collision_shape.shape = shape
+	
+	# Add a MeshInstance3D with the leverage mesh at the same location
+	var mesh_instance = MeshInstance3D.new()
+	var lever = $"../../lever"
+	var mesh = lever.mesh
+	if mesh:
+		mesh_instance.mesh = mesh
+	else:
+		print("Failed to load mesh from:", lever)
+		return
+
+	var corrected_rotation = Quaternion(Vector3(1, 0, 0), deg_to_rad(-90))
+	mesh_instance.transform = Transform3D(corrected_rotation, Vector3(0,-0.0025,0))
+	mesh_instance.name = "Lever_" + str(cell_no)
+
+	area.add_child(mesh_instance)
 
 func _on_Area3D_body_entered(body: Node):
 	print("Body that entered: ", body.name)
